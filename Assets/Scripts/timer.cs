fc75a569f3c45a446bd123b88683fb04ba2f04cf
Kaiken <pierrickgraton@gmail.com>
2023-02-07 10:40:04 +0100

    10h40

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-public class Timer : MonoBehaviour
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class timer : MonoBehaviour
 {
-    public float timeRemaining = 10;
+    public float timeRemaining = 300;
+    public TMP_Text timerText;
     void Update()
     {
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
         }
+        else
+        {
+            timeRemaining = 0;
+            SceneManager.LoadScene(2);
+        }
+        DisplayTime(timeRemaining);
+    }
+
+    void DisplayTime (float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
