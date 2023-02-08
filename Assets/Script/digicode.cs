7a1e289777676817e0361bee6f94db00f2065ca6
Kaiken <pierrickgraton@gmail.com>
2023-02-08 09:15:50 +0100

    09h15

--- Assets/Script/digicode.cs
+++ Assets/Script/digicode.cs
@@ -9,6 +9,12 @@ public class digicode : MonoBehaviour
     public string input;
     private float nmb_input;//nmb d'input nécessaire pour le mdp
     private float nmb_btn_click;//nmb d'input à la suite
+    public AudioClip openDoor;
+    public AudioClip digicode_correct;
+    public AudioClip beep_wrong;
+    public AudioSource digicode_source;
+    public AudioSource door_source;
+    public AudioSource Wdigicode_source;
     void Start()
     {
         nmb_btn_click = 0;
@@ -25,8 +31,13 @@ public class digicode : MonoBehaviour
                 input = "";
                 nmb_btn_click = 0;
                 //son bon code
+                digicode_source.clip = digicode_correct;
+                digicode_source.Play();
+
                 //lancement anim porte
                 Debug.Log("c'est bon");
+                door_source.clip = openDoor;
+                door_source.Play();
             }
 
             else
@@ -36,6 +47,8 @@ public class digicode : MonoBehaviour
                 input = "";
                 nmb_btn_click = 0;
                 //son mauvais code
+                Wdigicode_source.clip = beep_wrong;
+                Wdigicode_source.Play();
                 Debug.Log("c'est pas bon");
                 
             }
